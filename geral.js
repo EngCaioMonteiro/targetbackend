aa14e934f6745ead098db6f2a049c68dadb40203
Caio Oliveira Monteiro <engcaiomonteiro@gmail.com>
2025-01-11 17:56:54 -0300

    Ajustes

--- geral.js
+++ geral.js
@@ -23,7 +23,7 @@ function isFibonacci(num) {
     return false;
 }
 
-const numero = 21;
+const numero = 72;
 if (isFibonacci(numero)) {
     console.log(`${numero} pertence à sequência de Fibonacci.`);
 } else {
@@ -99,5 +99,5 @@ function reverseString(str) {
     return reversed;
 }
 
-const inputString = "Exemplo";
+const inputString = "O dia sempre brilha";
 console.log("String invertida:", reverseString(inputString));
